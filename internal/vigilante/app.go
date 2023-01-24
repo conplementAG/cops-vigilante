8dfa359fc9567440c2d5244609fe3f8fc3d12590
Denis Biondic <denis.biondic@conplement.de>
2023-01-24 09:39:37 +0100

    feat: verbose logging mode for gin & logrus

--- internal/vigilante/app.go
+++ internal/vigilante/app.go
@@ -5,10 +5,20 @@ import (
 	"github.com/conplementag/cops-vigilante/internal/vigilante/http"
 	"github.com/conplementag/cops-vigilante/internal/vigilante/metrics"
 	"github.com/conplementag/cops-vigilante/internal/vigilante/scheduler"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func Run() {
+	if viper.GetBool("verbose") {
+		gin.SetMode(gin.DebugMode)
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
 	metrics.Init()
 	scheduler.InitializeAndStart(viper.GetInt(cli.IntervalInSecondsFlag))
 	http.Start()
