66b4dbbdc8fdd6a434b3fd2b5dd11f19ba2b8e4c
Denis Biondic <denis.biondic@conplement.de>
2023-07-03 11:18:38 +0200

    feat: stricter k8s RBAC

--- internal/vigilante/tasks/snat/consts/consts.go
+++ internal/vigilante/tasks/snat/consts/consts.go
@@ -1,5 +1,7 @@
 package consts
 
+// NodeHealerNamespace we schedule in kube-system because this namespace is always there,
+// so we avoid some namespace-related problems
 const NodeHealerNamespace = "kube-system"
 
 const NodeHealedAnnotation = "cops-vigilante-snat-node-healed"
